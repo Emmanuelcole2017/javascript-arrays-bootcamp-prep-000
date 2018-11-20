9c1434e2e73e53c956fe6321c85845c4905e1730
Emmanuel Cole <emmanuel.c2011@gmail.com>
2018-11-20 21:50:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,6 +8,24 @@ function addElementToEndOfArray(array, element){
   return [...array, element]
 }
 
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
